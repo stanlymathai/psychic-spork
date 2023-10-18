423d3db9c7fba433bb485af3ac96d477280d85ce
stanlymathai <stanly@mathai.com>
2023-10-18 13:36:24 +0530

    chore; add env config files.

---
+++ src/configs/env.config/db.env.js
@@ -0,0 +1,10 @@
+module.exports = {
+  db_name: process.env.DB_NAME,
+  db_host: process.env.DB_HOST,
+
+  db_username: process.env.DB_USERNAME,
+  db_password: process.env.DB_PASSWORD,
+
+  db_socket_timeout: process.env.DB_SOCKET_TIMEOUT,
+  db_connection_timeout: process.env.DB_CONNECTION_TIMEOUT,
+};
