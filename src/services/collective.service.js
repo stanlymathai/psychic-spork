dabc695eaaacfc27db679c4ad2ea5f5926894b8a
stanlymathai <stanly@mathai.com>
2023-10-19 13:34:33 +0530

    chore; add user and collective service fn files

---
+++ src/services/collective.service.js
@@ -0,0 +1,19 @@
+const ERROR_MESSAGES = require('../utils/errorMessage.util');
+
+async function createCollective(collective) {
+  return;
+}
+
+async function getCollectiveById(collectiveId) {
+  return;
+}
+
+async function getCollectiveByName(name) {
+  return;
+}
+
+module.exports = {
+  createCollective,
+  getCollectiveById,
+  getCollectiveByName,
+};
---
+++ src/services/user.service.js
@@ -0,0 +1,19 @@
+const ERROR_MESSAGES = require('../utils/errorMessage.util');
+
+async function createUser(user) {
+  return;
+}
+
+async function getUserById(userId) {
+  return;
+}
+
+async function getUserByEmail(email) {
+  return;
+}
+
+module.exports = {
+  createUser,
+  getUserById,
+  getUserByEmail,
+};
