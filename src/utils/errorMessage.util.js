bbeccfa787581d7d930596986e17d4646ee48aaf
stanlymathai <stanly@mathai.com>
2023-10-19 13:32:25 +0530

    chore; add error messages and http status util file

---
+++ src/utils/errorMessage.util.js
@@ -0,0 +1,11 @@
+const ERROR_MESSAGES = {
+  //collective
+  INVALID_COLLECTIVE_ID: 'Invalid collective id',
+  COLLECTIVE_NOT_FOUND: 'Collective not found',
+
+  //user
+  INVALID_USER_ID: 'Invalid user id',
+  USER_NOT_FOUND: 'User not found',
+};
+
+module.exports = ERROR_MESSAGES;
